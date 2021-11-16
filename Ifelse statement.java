df0f0545e1306ac5d9ff5fcd8dfa170b313f2081
ajay2prajapati <48910435+ajay2prajapati@users.noreply.github.com>
2021-11-15 20:39:06 -0800

    Add files via upload

---
+++ Ifelse statement.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+int i,m=0,flag=0;      
+ int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Age is greater than 18");  
+    }  
+	}
+}
